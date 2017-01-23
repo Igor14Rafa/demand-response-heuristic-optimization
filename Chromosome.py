e18553d91c2d4561cb55c97d65e2581247d8d2f8
Igor <noemail@upi.br>
2017-01-23 16:44:21 -0300

    Adding initial files.

---
+++ Chromosome.py
@@ -0,0 +1,69 @@
+from random import *
+
+
+class Chromosome():
+    def __init__(self, chromosome_size, crossover_tax, mutation_chance):
+        self.chromosome_size = chromosome_size
+        self.crossover_tax = crossover_tax
+        self.mutation_chance = mutation_chance
+        self.relative_fitness = 0
+        self.absolute_fitness = 0
+	self.consumption = []
+	self.value = []
+        #initialize the chromossome with the bits representing the devices
+        for i in range(self.chromosome_size):
+		    if random() > 0.5:
+			    self.value.append(1)
+		    else:
+			    self.value.append(0)
+
+
+    """ Implementation of the default crossover operator.
+         This crossover calculates a cutpoint
+         and split the two fathers at this point,
+         joining different parts of each one to make a new son.
+    """
+    def crossover(self, chromosome_2):
+        sons = []
+        cutpoint = int(random() * self.chromosome_size)
+        son_1 = Chromosome(self.chromosome_size, self.crossover_tax, self.mutation_chance)
+	    if random() < self.crossover_tax:
+        	   son_1.value = self.value[0: cutpoint] + chromosome_2.value[cutpoint: (len(self.value))]
+	else:
+		son_1.value = self.value
+        sons.append(son_1)
+        son_2 = Chromosome(self.chromosome_size, self.crossover_tax, self.mutation_chance)
+	    if random() < self.crossover_tax:
+        	   son_2.value = chromosome_2.value[0: cutpoint] + self.value[cutpoint: (len(self.value))]
+	else:
+		son_2.value = self.value
+        sons.append(son_2)
+        return sons
+
+    """Implementation of the default mutation operator.
+        This operator calculates if the "mutation" will occur
+        in any of the "genes"(devices) in chromosome,
+        based on the mutation chance.
+    """
+    def mutation(self):
+        for i in range(self.chromosome_size):
+            if random() < self.mutation_chance:
+                self.value[i] = abs(self.value[i] - 1)
+    
+   def get_consumption(self, filename):
+        f = open(filename, "r")
+	aux_nums = []
+        for line in f:
+            aux_nums.append(line)
+	aux_nums.pop()
+	for i in aux_nums:
+	    self.consumption.append(float(i))
+        f.close()
+
+
+    def __str__(self):
+        return "Value=>{0} : Absolute Fitness=>{1}; Relative Fitness=>{2}\n".format(self.value, self.absolute_fitness, self.relative_fitness)
+
+if __name__ == "__main__":
+    test = Chromosome(240, 0.6, 0.1)
+    print test.value
---
+++ GA.py
@@ -0,0 +1,179 @@
+from Chromosome import *
+from copy import *
+
+
+
+class GA():
+    def __init__(self, num_gen=100, pop_size=50, cross_tax=0.6,
+                 mut_chance=0.1, chrom_size=240, tourn_size=3, selection=1, n_dev=10):
+        self.num_generation = num_gen
+        self.pop_size = pop_size
+        self.crossover_tax = cross_tax
+        self.mutation_chance = mut_chance
+        self.chromosome_size = chrom_size
+        self.tournament_size = tourn_size
+	self.num_devices = n_dev
+        self.prices = {}
+        self.population = []
+        self.selection = selection
+        self.error = 0.01
+
+    """ Creates the dict "prices" with the info of the problem,
+        given by the file "filename".
+    """
+    def get_prices(self, filename):
+        f = open(filename, "r")
+        key = 1
+        for line in f:
+            self.prices[key] = map(int, line.split())
+            key += 1
+        f.close()
+
+    """ Initialize the population."""
+    def init_pop(self):
+        for i in range(self.pop_size):
+            chromosome = Chromosome(self.chromosome_size, self.crossover_tax, self.mutation_chance)
+            self.verify_valid_son(chromosome)
+            self.population.append(chromosome)
+
+    """ Calculate the fitness for each chromosome in "population",
+        based on the route represented in the chromosome.
+    """
+    def fitness(self, population):
+        index = 0
+        for chromosome in population:
+            chromosome.absolute_fitness = 0 #Total consumption of the configuration represented by the chromosome
+            chromosome.relative_fitness = 0 #Value used for minimize the problem (Inverse of the absolute_fitness)
+	    for i in range(1, 25):
+            	for i in range(1, num_devices + 1):
+			            chromosome.absolute_fitness += 0  #Thinking about the iteration per hour
+                	    chromosome.absolute_fitness += self.prices[int(chromosome.value[i])][int(chromosome.value[i + 1]) - 1] #At each two sequential "prices" in chromosome, adds the edge value to the fitness.
+            		    chromosome.relative_fitness = 1.0/chromosome.absolute_fitness
+
+    """ Verify if the "chromosome" is a valid solution for the Traveller Salesman Problem."""
+    def verify_valid_son(self, chromosome):
+        generate_new_son = False
+        for i in range(len(chromosome.value) - 1):
+            if (self.prices[int(chromosome.value[i])][int(chromosome.value[i + 1]) - 1] == -1): #Verify if each route in chromosome really exist.
+                generate_new_son = True
+        if(chromosome.value[0] != chromosome.value[-1]): #Verify if the route in chromosome returns to its origin.
+            generate_new_son = True
+        for i in range(0, self.chromosome_size - 1):
+            for j in range(i + 1, self.chromosome_size):
+                if chromosome.value[i] == chromosome.value[j]: #Verify if each city (except origin) is visited only once.
+                    generate_new_son = True
+        if generate_new_son == True:
+            chromosome.value = sample(xrange(1, self.chromosome_size + 1),  self.chromosome_size)
+            chromosome.value.append(chromosome.value[0])
+            self.verify_valid_son(chromosome)
+
+    """ Implementation of the tournament selection operator."""
+    def tournament(self):
+        fathers = []
+        best_index = 0
+        best_fitness = 0
+        for i in range(self.tournament_size):
+            fathers.append(self.population[int(random() * self.pop_size)])
+            if fathers[i].relative_fitness > best_fitness:
+                best_index = i
+                best_fitness = fathers[i].relative_fitness
+        return fathers[best_index]
+
+    """ Implementation of the roulette selection ooperator. """
+    def roulette(self):
+        index = 0
+        aux_sum = 0
+        upper_boundary = random()*self.sum_all_fitness()
+        i = 0
+        while(aux_sum < upper_boundary) and (i < self.pop_size):
+            aux_sum += self.population[i].relative_fitness
+            i += 1
+        index = i - 1
+        return self.population[index]
+
+    """ Returns the sum of all fitness in population."""
+    def sum_all_fitness(self):
+        sum_fitness = 0
+        for chromosome in self.population:
+            sum_fitness += chromosome.relative_fitness
+        return sum_fitness
+
+    """ Gets the best chromosome in population, according to its relative fitness."""
+    def get_best(self, population):
+        index_best = 0
+        fitness_best = 0
+        for i in range(len(population)):
+            if population[i].relative_fitness > fitness_best:
+                index_best = i
+                fitness_best = population[i].relative_fitness
+        return index_best
+
+    """ Updates the population, creating sons by crossover of two fathers (chromosomes),
+        and adding the sons to the new population.
+    """
+    def generation(self):
+        new_population = []
+        index_fathers = 1
+        index_sons = 1
+        if self.selection == 0:
+            print "Selection Mode: Roulette"
+        else:
+            print "Selection Mode: Tournament"
+        while(len(new_population) < self.pop_size):
+            if self.selection == 0:
+                father1 = self.roulette()
+                father2 = self.roulette()
+                print "Selected Fathers:\n\n#{0}: {1}\n#{2}: {3}\n".format(index_fathers, father1, index_fathers + 1, father2)
+                sons = father1.crossover(father2)
+                index_fathers += 2
+            else:
+                father1 = self.tournament()
+                father2 = self.tournament()
+                print "Selected Fathers:\n\n#{0}: {1}\n#{2}: {3}\n".format(index_fathers, father1, index_fathers + 1, father2)
+                sons = father1.crossover(father2)
+                index_fathers += 2
+            for son in sons:
+                son.mutation()
+                self.verify_valid_son(son)
+                print "Adding son #{0}:{1} to the population.\n".format(index_sons, son)
+                index_sons += 1
+                new_population.append(son)
+        self.population = deepcopy(new_population)
+        self.fitness(self.population)
+
+    """Calculates the media of the fitness in population."""
+    def calc_media(self, population):
+        media = 0.0
+        for chromosome in population:
+            media += chromosome.relative_fitness
+        media = media / len(population)
+        return media
+
+    """ Verify if the population is converging,
+        calculating the difference between the media and the best chromosome in population.
+    """
+    def verify_convergence(self):
+        convergence = False
+        index_best = self.get_best(self.population)
+        if (self.population[index_best].relative_fitness - self.calc_media(self.population)) < self.error:
+            convergence = True
+        return convergence
+
+    """ The main process of the genetic algorithm."""
+    def process(self):
+        self.get_prices("cidades7.txt")
+        self.init_pop()
+        self.fitness(self.population)
+        generation_index = 1
+        while(generation_index < self.num_generation):
+            print "\n\n\t\t\tGENERATION {0}\n".format(generation_index)
+            generation_index += 1
+            self.generation()
+        print "\n\n\t\tBEST SOLUTION FOUND AT GENERATION {0}.\nBest is : {1}".format(generation_index, self.population[self.get_best(self.population)])
+        print "Number of Generations: {0}.\n".format(generation_index)
+        print "Population Converged.\n"
+        return self.population[self.get_best(self.population)]
+
+if __name__ == "__main__":
+    test = GA(10, 5, 0.5, 0.1, 7, 3, 1)
+    test.process()
